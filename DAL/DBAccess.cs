c3620aa2ae3b0b2875d19063d2948e803526d4d9
Luis David <cusiluis04@gmail.com>
2023-03-02 13:49:44 -0500

    Avance del acceso a la DB y formularios

--- DAL/DBAccess.cs
+++ DAL/DBAccess.cs
@@ -1,12 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
 
 namespace DAL
 {
-    internal class DBAccess
+    public class DBAccess
     {
+        private SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["sisfarmacia"].ConnectionString);
+
+        public SqlConnection getConexion()
+        {
+            return this.conn;
+        }
+
+        public void conectar()
+        {
+            if(this.conn.State == ConnectionState.Closed)
+            {
+                this.conn.Open();
+            }
+        }
+
+        public void desconectar()
+        {
+            if (this.conn.State == ConnectionState.Open)
+            {
+                this.conn.Close();
+            }
+        }
+
+
+        public DataTable getDataGrid(string spu, int estado)
+        {
+            DataTable dt = new DataTable();
+            conectar();
+
+            SqlCommand sqlCommand = new SqlCommand(spu, getConexion());
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+
+            sqlCommand.Parameters.AddWithValue("@estado", estado);
+            dt.Load(sqlCommand.ExecuteReader());
+
+            desconectar();
+            return dt;
+
+
+        }
+
+
+
     }
 }
