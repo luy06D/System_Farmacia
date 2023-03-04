7ac6cf138108ef02f63badd0ad8115801912538b
Jesus <jesuscamachoforever@gmail.com>
2023-03-03 20:51:09 -0500

    Ultima version

--- DESIGNER/Formularios/FrmLogin.Designer.cs
+++ DESIGNER/Formularios/FrmLogin.Designer.cs
@@ -38,21 +38,21 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(548, 172);
+            this.textBox1.Location = new System.Drawing.Point(171, 161);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(139, 20);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(548, 239);
+            this.textBox2.Location = new System.Drawing.Point(171, 231);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(139, 20);
             this.textBox2.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(564, 279);
+            this.button1.Location = new System.Drawing.Point(191, 291);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(107, 22);
             this.button1.TabIndex = 2;
@@ -63,7 +63,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(516, 34);
+            this.label1.Location = new System.Drawing.Point(138, 40);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(196, 25);
             this.label1.TabIndex = 3;
@@ -73,7 +73,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(545, 141);
+            this.label2.Location = new System.Drawing.Point(204, 124);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(54, 17);
             this.label2.TabIndex = 4;
@@ -83,7 +83,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(545, 219);
+            this.label3.Location = new System.Drawing.Point(204, 211);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(84, 17);
             this.label3.TabIndex = 5;
@@ -94,7 +94,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SlateBlue;
-            this.ClientSize = new System.Drawing.Size(788, 352);
+            this.ClientSize = new System.Drawing.Size(491, 352);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
