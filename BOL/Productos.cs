a4070e45a49210bc4d11464e92ba83f371370fcd
Luis David <cusiluis04@gmail.com>
2023-03-03 20:06:34 -0500

    Metodos de producto y ventas

--- BOL/Productos.cs
+++ BOL/Productos.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Globalization;
+using DAL;
 
 namespace BOL
 {
-    internal class Productos
+    public class Productos
     {
+
+        DBAccess acceso = new DBAccess();
+
+        public void registrarProductos(
+            int idlaboratorio, int idcategoria, string nombreproducto, string descripcion,
+            string cantidad, string precio, string fechaproduccion, string fechavencimiento,
+            string numlote, string recetamedica)
+        {
+            SqlCommand command = new SqlCommand("",acceso.getConexion());
+            command.CommandType = CommandType.StoredProcedure;
+            acceso.conectar();
+
+            command.Parameters.AddWithValue("@idlaboratorio", idlaboratorio);
+            command.Parameters.AddWithValue("@idcategoria", idcategoria );
+            command.Parameters.AddWithValue("@nombreproducto", nombreproducto);
+            command.Parameters.AddWithValue("@descripcion", descripcion );
+            command.Parameters.AddWithValue("@cantidad", cantidad );
+            command.Parameters.AddWithValue("@precio", precio );
+            command.Parameters.AddWithValue("@fechaproduccion", fechaproduccion );
+            command.Parameters.AddWithValue("@fechavencimiento", fechaproduccion );
+            command.Parameters.AddWithValue("@numlote", numlote );
+            command.Parameters.AddWithValue("@recetamedica", recetamedica );
+
+            command.ExecuteNonQuery();
+            acceso.desconectar();
+        }
+        
     }
 }
--- BOL/Ventas.cs
+++ BOL/Ventas.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace BOL
 {
-    internal class Ventas
+    public class Ventas
     {
+        DBAccess acceso = new DBAccess();
+
+        public DataTable buscarProducto(int idproducto)
+        {
+            DataTable data = new DataTable();
+            SqlCommand command = new SqlCommand("", acceso.getConexion());
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@idproducto", idproducto);
+
+            acceso.conectar();
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            adapter.Fill(data);
+            acceso.desconectar();
+            return data;
+        }
+
     }
 }
