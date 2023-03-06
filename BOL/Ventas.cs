ccf68105c95b05db701c3c59c6347474cab0c518
fabriziobarriossaavedra <fabriziobarrios22@gmail.com>
2023-03-06 18:51:26 -0500

    ventas

--- BOL/Ventas.cs
+++ BOL/Ventas.cs
@@ -6,58 +6,37 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
-<<<<<<< HEAD
-using DAL;
 using ENTITIES;
-using System.Net.Http.Headers;
-=======
->>>>>>> 25f5dd3011dacacaa5c6fa71c426489be9d0ca4e
+using System.Web;
 
-namespace BOL
-{
-    public class Ventas
-    {
+namespace BOL{
+    public class Ventas{
         DBAccess acceso = new DBAccess();
 
-<<<<<<< HEAD
-
-        public DataTable listar()
-        {
+        public DataTable listar(){
             return acceso.getDataGrid("", 1);
         }
 
-
-        public int registrarVentas(EVentas entidad)
-        {
-
-
+        public int registrarVentas(EVentas entidad){
             int registrosAfectados = 0;
-
             acceso.conectar();
-
             SqlCommand sqlCommand = new SqlCommand("", acceso.getConexion());
             sqlCommand.CommandType = CommandType.StoredProcedure;
-
             sqlCommand.Parameters.AddWithValue("@nombreProducto", entidad.nombreProducto);
             sqlCommand.Parameters.AddWithValue("@descripcion", entidad.descripcion);
             sqlCommand.Parameters.AddWithValue("@precio", entidad.precio);
             sqlCommand.Parameters.AddWithValue("@cantidad", entidad.cantidad);
             sqlCommand.Parameters.AddWithValue("@fechaVencimiento", entidad.fechaVencimiento);
             sqlCommand.Parameters.AddWithValue("@recetaMedica", entidad.recetaMedica);
-
             acceso.desconectar();
-
             return registrosAfectados;
-
         }
-=======
-        public DataTable buscarProducto(int idproducto)
-        {
+
+        public DataTable buscarProducto(int idproducto){
             DataTable data = new DataTable();
-            SqlCommand command = new SqlCommand("", acceso.getConexion());
+            SqlCommand command = new SqlCommand(" ", acceso.getConexion());
             command.CommandType = CommandType.StoredProcedure;
             command.Parameters.AddWithValue("@idproducto", idproducto);
-
             acceso.conectar();
             SqlDataAdapter adapter = new SqlDataAdapter(command);
             adapter.Fill(data);
@@ -65,8 +44,29 @@ namespace BOL
             return data;
         }
 
->>>>>>> 25f5dd3011dacacaa5c6fa71c426489be9d0ca4e
-    }
+        public int actualizarProductos(EVentas entidad){
+            int actualizarproducto = 0;
+            acceso.conectar();
+            SqlCommand sqlCommand = new SqlCommand(" ", acceso.getConexion());
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+            sqlCommand.Parameters.AddWithValue("@nombreProducto", entidad.nombreProducto);
+            sqlCommand.Parameters.AddWithValue("@descripcion", entidad.descripcion);
+            sqlCommand.Parameters.AddWithValue("@precio", entidad.precio);
+            sqlCommand.Parameters.AddWithValue("@cantidad", entidad.cantidad);
+            sqlCommand.Parameters.AddWithValue("@fechaVencimiento", entidad.fechaVencimiento);
+            acceso.desconectar();
+            return actualizarproducto;
+        }
 
-  
+        public int eliminarProductos(EVentas entidad)
+        {
+            int eliminarProductos = 1;
+            acceso.conectar();
+            SqlCommand sqlCommand = new SqlCommand(" ", acceso.getConexion());
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+            sqlCommand.Parameters.AddWithValue("@idproducto", entidad.idproducto);
+            acceso.desconectar();
+            return eliminarProductos;
+        }
+    }
 }
