262d0f62a27927f5cd75048f2bd6a9c9fcd7c756
Luis David <cusiluis04@gmail.com>
2023-03-07 08:08:13 -0500

    Revert "Revert "Creacion del formulario para crear el codigo de barras""
    This reverts commit 5f4deb4f6886559e02c9a17c2c13c81c16ff0c9c.

---
+++ DESIGNER/Test/FrmCodigo.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace DESIGNER.Test
+{
+    partial class FrmCodigo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNamePro = new System.Windows.Forms.TextBox();
+            this.txtnumCodigo = new System.Windows.Forms.TextBox();
+            this.pictureCodigo = new System.Windows.Forms.PictureBox();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.comboTipo = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureCodigo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre del producto:";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.AutoSize = true;
+            this.txtCodigo.Location = new System.Drawing.Point(43, 99);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(43, 13);
+            this.txtCodigo.TabIndex = 1;
+            this.txtCodigo.Text = "Codigo:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tipo:";
+            // 
+            // txtNamePro
+            // 
+            this.txtNamePro.Location = new System.Drawing.Point(46, 56);
+            this.txtNamePro.Name = "txtNamePro";
+            this.txtNamePro.Size = new System.Drawing.Size(208, 20);
+            this.txtNamePro.TabIndex = 3;
+            // 
+            // txtnumCodigo
+            // 
+            this.txtnumCodigo.Location = new System.Drawing.Point(46, 115);
+            this.txtnumCodigo.Name = "txtnumCodigo";
+            this.txtnumCodigo.Size = new System.Drawing.Size(208, 20);
+            this.txtnumCodigo.TabIndex = 5;
+            // 
+            // pictureCodigo
+            // 
+            this.pictureCodigo.Location = new System.Drawing.Point(69, 281);
+            this.pictureCodigo.Name = "pictureCodigo";
+            this.pictureCodigo.Size = new System.Drawing.Size(300, 100);
+            this.pictureCodigo.TabIndex = 6;
+            this.pictureCodigo.TabStop = false;
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(56, 234);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(329, 31);
+            this.btnGenerar.TabIndex = 7;
+            this.btnGenerar.Text = "Generar Codigo";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(56, 422);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(329, 34);
+            this.btnGuardar.TabIndex = 8;
+            this.btnGuardar.Text = "Guardar codigo";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // comboTipo
+            // 
+            this.comboTipo.FormattingEnabled = true;
+            this.comboTipo.Location = new System.Drawing.Point(46, 183);
+            this.comboTipo.Name = "comboTipo";
+            this.comboTipo.Size = new System.Drawing.Size(208, 21);
+            this.comboTipo.TabIndex = 9;
+            // 
+            // FrmCodigo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 483);
+            this.Controls.Add(this.comboTipo);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.pictureCodigo);
+            this.Controls.Add(this.txtnumCodigo);
+            this.Controls.Add(this.txtNamePro);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmCodigo";
+            this.Text = "FrmCodigo";
+            this.Load += new System.EventHandler(this.FrmCodigo_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureCodigo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label txtCodigo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNamePro;
+        private System.Windows.Forms.TextBox txtnumCodigo;
+        private System.Windows.Forms.PictureBox pictureCodigo;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.ComboBox comboTipo;
+    }
+}
